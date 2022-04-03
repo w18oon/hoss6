76ae25443348f8a3f09ee6ee8250dc185241eb2f
Wiboon Supachitranan <w18oon@gmail.com>
2022-04-03 16:07:18 +0700

    Add MUI

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+// import '../styles/globals.css';
+import { theme } from '../theme';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
